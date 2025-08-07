c9972d1da6390d6df1ba9e603f87fad4568f69fb
Moises <moisesdavidlara1@gmail.com>
2025-08-07 04:10:40 -0400

    add create students page

---
+++ src/app/(home)/estudiantes/create/page.tsx
@@ -0,0 +1,11 @@
+
+import { CreateStudentForm } from "@/components/students/forms/create-student-form"
+
+
+export default function CreateStudentsPage() {
+    return (
+        <div className=""> 
+            <CreateStudentForm/>
+        </div>
+    )
+}
