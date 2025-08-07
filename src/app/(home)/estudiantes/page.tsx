0026819d379f7ef1c83b2b6fc8bcdc86727be44c
Moises <moisesdavidlara1@gmail.com>
2025-08-07 00:56:39 -0400

    add students page

---
+++ src/app/(home)/estudiantes/page.tsx
@@ -0,0 +1,10 @@
+import {StudentsPage} from "@/components/students/students-page"
+
+
+export default function Page() {
+    return (
+        <div className="min-h-screen ">
+            <StudentsPage/>   
+        </div>
+    )
+}
