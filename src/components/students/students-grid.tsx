ab8b7c002b2881b78046dd249f96ce1bb44263d7
Moises <moisesdavidlara1@gmail.com>
2025-08-09 12:47:18 -0400

    add the logic of get student info to the component that show the info for use suspense in the parent

--- src/components/students/students-grid.tsx
+++ src/components/students/students-grid.tsx
@@ -1,13 +1,47 @@
 import { StudentCard } from "./student-card"
 import type { Student } from "./types"
+import { StudentFilters } from "./types"
+import { getStudentsWithInfo } from "@/components/students/lib/fetch-data"
 
-interface StudentsGridProps {
-  students: Student[]
-  onViewDetails?: (student: Student) => void
-}
 
-export function StudentsGrid({ students, onViewDetails }: StudentsGridProps) {
-  if (students.length === 0) {
+export async function StudentsGrid({ searchParams }: { searchParams: StudentFilters }) {
+  const { search = '', section = "todos", status = "activo", year = "todos" } = searchParams
+  const students: Student[] = await getStudentsWithInfo({ search, section, status, year })
+  const procesedStrudents = students.map((student) => {
+    return {
+      ...student,
+      detalle_materias: (student.detalle_materias as String).split(" | ").map((grade: any) => {
+        const [subject, average, notes,] = grade.split(", ");
+        const subjectValue = subject.split(": ")[1];
+        const averageValue = average.split(": ")[1];
+        const notesValue = notes.split(": ")[1];
+
+
+
+        return {
+          subject: subjectValue,
+          average: averageValue,
+          notes: JSON.parse(notesValue),
+        }
+
+
+      }),
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if (procesedStrudents.length === 0) {
     return (
       <div className="text-center py-8 sm:py-12">
         <div className="text-gray-400 dark:text-gray-600 mb-4">
@@ -26,12 +60,11 @@ export function StudentsGrid({ students, onViewDetails }: StudentsGridProps) {
   }
 
   return (
-    <div className="grid gap-4 sm:gap-6 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-      {students.map((student) => (
+    <div className="grid gap-4 sm:gap-6 grid-cols-1 p-4 lg:grid-cols-2 xl:grid-cols-3">
+      {procesedStrudents.map((student) => (
         <StudentCard
           key={student.id}
           student={student}
-          onViewDetails={onViewDetails}
         />
       ))}
     </div>
