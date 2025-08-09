dd4ea023cc4e10fada563f52141120b5f7966a39
Moises <moisesdavidlara1@gmail.com>
2025-08-09 06:38:32 -0400

    modify for add login with db, i need to refactor and extract the login then

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,30 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { sql } from "@/lib/db";
+import { User } from "@/utils/types.d";
+
+async function getUserByEmailAndRole({
+  email,
+  role,
+}: {
+  email: string;
+  role: "Docente" | "Admin";
+}): Promise<User | null> {
+  try {
+    const user = (await sql.query(
+      `SELECT u.id_usuario AS user_id, email, nombre_rol AS rol,fecha_creacion AS created_at, contrasena_hash AS hashed_password FROM usuarios u 
+      JOIN usuario_roles ur ON ur.id_usuario = u.id_usuario 
+      JOIN roles r ON r.id_rol = ur.id_rol 
+      WHERE email = $1 AND r.nombre_rol = $2 AND activo = TRUE`,
+      [email, role]
+    )) as User[];
+
+    return user[0] ?? null;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -8,23 +33,36 @@ export const authOptions: AuthOptions = {
       // `credentials` se usa para generar un formulario en la página de inicio de sesión.
       // Puedes especificar los campos que esperas que se envíen.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: {
+          label: "email",
+          type: "email",
+          placeholder: "jsmith@example.com",
+        },
         password: { label: "Password", type: "password" },
+        role: { label: "role", type: "text", placeholder: "docente" },
       },
       async authorize(credentials, req) {
+        const { email, role, password } = credentials as {
+          email: string;
+          password: string;
+          role: "Docente" | "Admin";
+        };
 
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+   
+        const myUser = await getUserByEmailAndRole({ email, role });
+        if (!myUser) return null;
 
-        console.log({credentials,req} );
+        // comprobar contraseña
+        if (password !== myUser.hashed_password) return null;
+
+        const {email: mail,rol,user_id} = myUser
+
+
         
-
-        if (user) {
-     
-          return user; // Autenticación exitosa
-        } else {
-          // Si devuelves `null`, se mostrará un error al usuario.
-          console.log("Credenciales inválidas:", credentials);
-          return null;
+        return {
+          id: user_id,
+          role: rol,
+          email: mail,     
         }
       },
     }),
@@ -32,15 +70,41 @@ export const authOptions: AuthOptions = {
 
   session: {
     strategy: "jwt",
+   
   },
   pages: {
     signIn: "/login",
     error: "/error", // Error de la autenticación
   },
   secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (!user) return token;
+
+      token.id = user.id as number;
+      token.role = user.role
+      console.log("jwt ",token);
+      
+
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+
+      session.user.role = token.role
+      session.user.email = token.email ?? ""
+      session.user.id = token.id
+      
+      console.log("session ",session);
+
+  
+     
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
-
 
 export { handler as GET, handler as POST };
