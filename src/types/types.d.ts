c8bf85454377e02615894281bb2c30038141b9c4
Moises <moisesdavidlara1@gmail.com>
2025-08-11 17:27:54 -0400

    create a new types file in /types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -10,3 +10,50 @@ export interface User {
 
 
 
+export interface Student {
+  id: string
+  nombres: string
+  apellidos: string
+  avatar?: string
+  ano: string
+  seccion: string
+  detalle_materias: Grade[] | string
+  promedio_general: number
+  status: "activo" | "inactivo"
+ 
+ 
+}
+
+export interface Grade {
+  subject: string
+  average: number,
+  notes: number[]
+  date?: string
+  type?: "exam" | "homework" | "project" | "quiz"
+}
+
+export interface StudentFilters {
+  year?: string
+  section?: string
+  status?: string
+  search?: string
+  años?: AñoData[]
+  secciones? : SeccionData[]
+}
+
+
+export interface AñoData {
+  nombre: string;
+  id: number;
+}
+
+export interface SeccionData {
+  nombre: string;
+  id: number;
+}
+
+
+export interface LapsoData {
+    id: number;
+    nombre: string;
+}
