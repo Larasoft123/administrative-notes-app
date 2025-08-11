54fef83e378a23d34d79905e09d856fff57d0d4f
Moises <moisesdavidlara1@gmail.com>
2025-08-11 17:24:00 -0400

    add link for create new evaluation

--- src/components/dashboard/dashboard-header.tsx
+++ src/components/dashboard/dashboard-header.tsx
@@ -1,17 +1,23 @@
 import { Button } from "@/components/ui/button"
 import { UserCheck } from "lucide-react"
+import Link from "next/link"
 
-export function DashboardHeader({profName = "unknow"}:{profName?:string}) {
+
+export function DashboardHeader({ profName = "unknow" }: { profName?: string }) {
   return (
     <div className="flex items-center justify-between">
       <div>
         <h1 className="text-3xl font-bold text-gray-900 dark:text-white">Mi Dashboard</h1>
         <p className="text-gray-600 dark:text-gray-400 mt-1">Prof. {profName}</p>
       </div>
-      <Button className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700">
-        <UserCheck className="mr-2 h-4 w-4" />
-        Nueva Evaluación
-      </Button>
+
+
+      <Link href={"/evaluaciones/new"}>
+        <Button className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700">
+          <UserCheck className="mr-2 h-4 w-4" />
+          Nueva Evaluación
+        </Button>
+      </Link>
     </div>
   )
 }
