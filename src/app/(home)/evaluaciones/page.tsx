5de7ff63bf1fe85f254fbbe7597866df0f2427d7
Moises <moisesdavidlara1@gmail.com>
2025-08-11 17:18:35 -0400

    add component evaluations page in the page

---
+++ src/app/(home)/evaluaciones/page.tsx
@@ -0,0 +1,15 @@
+import {EvaluationsPage} from "@/components/evaluaciones/evaluations-page"
+
+export default function EvalucionesPage() {
+    return (
+        <div className="min-h-screen">
+            <EvaluationsPage />
+        </div>
+    )
+}
+
+
+
+
+
+
