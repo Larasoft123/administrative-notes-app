7b8a72136635d40c5e2d4085bb6ede07b8285e1e
Moises <moisesdavidlara1@gmail.com>
2025-08-11 17:32:24 -0400

    move the button inside link

---
+++ src/components/evaluaciones/evaluations-header.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button"
+import { UserPlus } from 'lucide-react'
+import Link from "next/link"
+
+
+export function EvaluationsHeader({  }) {
+  return (
+    <div className="flex sm:flex-row flex-col gap-y-4  items-center sm:justify-between">
+      <div>
+        <h1 className="text-3xl text-center text-balance sm:text-start font-bold text-gray-900 dark:text-white">Evaluaciones</h1>
+        <p className="text-gray-600 text-center text-balance sm:text-start dark:text-gray-400 mt-1">
+          Gestiona la información de tus evaluaciones
+        </p>
+      </div>
+      <div className="flex sm:flex-row flex-wrap sm:flex-nowrap gap-y-4 justify-center items-center space-x-2">
+        <Link href="/evaluaciones/new">
+        <Button className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700" >
+          <UserPlus className="mr-2 h-4 w-4" />
+          Nueva Evaluacion
+        </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
