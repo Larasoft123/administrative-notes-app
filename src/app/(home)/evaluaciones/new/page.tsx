5b949c8e0591e1e31e9d7d5f01bb96f5c6319452
Moises Lara <Moisesdavidlara1@gamil.com>
2025-08-25 12:55:31 -0400

    add search all data and promise.all

--- src/app/(home)/evaluaciones/new/page.tsx
+++ src/app/(home)/evaluaciones/new/page.tsx
@@ -1,28 +1,43 @@
 import { EvaluationForm } from "@/components/evaluaciones/forms/create-evaluation-form"
-import {getStudentsPageData} from "@/lib/api/students"
+import { getStudentsPageData } from "@/lib/api/students"
 import { getSessionServer } from "@/utils/session"
 import { getLapsos } from "@/lib/api/lapsos"
+import { getSubjects } from "@/lib/api/materias"
+import { getPeriodos } from "@/lib/api/periodos-escolares"
+import { getTiposEvaluaciones } from "@/lib/api/tipos-evaluaciones"
+
+
+
 export default async function CreateEvaluationPage() {
   const session = await getSessionServer()
-  if(!session){
+  if (!session) {
     return <div>No se ha iniciado sesión</div>
   }
-  const {user : {id}} = session
+  const { user: { id, role } } = session
 
 
-  const [years, sections] = await getStudentsPageData({id})
-  const lapsos = await getLapsos({userId: id})
+  const [years, sections] = await getStudentsPageData({ id })
 
-  
+  const [lapsos, materias, periodos, tiposEvaluaciones] = await Promise.all([
+    getLapsos({ userId: id }),
+    getSubjects({ userId: id, role }),
+    getPeriodos(),
+    getTiposEvaluaciones()
+  ])
 
 
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
   return (
     <div>
-        <EvaluationForm anos={years} secciones={sections} lapsos={lapsos}  />
+      <EvaluationForm tiposEvaluaciones={tiposEvaluaciones} periodos_escolares={periodos} materias={materias} anos={years} secciones={sections} lapsos={lapsos} />
     </div>
   )
 }
