b61f5f8b3d2cb3a9b0ec9657a7a6c0b26e87458e
Moises Lara <Moisesdavidlara1@gamil.com>
2025-08-28 19:15:24 -0400

    refactor: simplify evaluation fetching in UpcomingActivitys component

--- src/components/dashboard/upcoming-activitis.tsx
+++ src/components/dashboard/upcoming-activitis.tsx
@@ -1,37 +1,15 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { ActivityItem } from "@/components/dashboard/activity-item"
+import { getEvaluations } from "@/lib/api/evaluaciones"
 
 
-interface Evaluation {
-  id_evaluacion: number
-  nombre_evaluacion: string
-  descripcion_evaluacion: string
-  nombre_ano : string
-  nombre_seccion : string
-  tipo_evaluacion : string
 
-}
+export async function UpcomingActivitys({ userId }: { userId: number }) {
 
-const getEvaluationsData = async ({id}:{id:number}) => {
+  const evaluations = await getEvaluations({ id: userId })
 
-    try {
-        const res = await fetch(`http://localhost:3000/api/evaluaciones?userId=${id}`)
-        const data = (await res.json()) as Evaluation[]
-        return data    
-    } catch (error) {
-        console.error(error)
-        return []
-        
-    }
 
-}
 
-export async function UpcomingActivitys({userId}: {userId: number}) {
-
-  const evaluations = await getEvaluationsData({id: userId})
-  
-
-  
   return (
     <Card>
       <CardHeader>
@@ -40,12 +18,12 @@ export async function UpcomingActivitys({userId}: {userId: number}) {
       </CardHeader>
       <CardContent>
         <div className="space-y-4">
-        {
-          evaluations.length === 0 ? <h4 className="text-center">No hay evaluaciones pendientes</h4> :
-          evaluations.map((evaluation,) => (
-            <ActivityItem key={evaluation.id_evaluacion}  nombre={evaluation.nombre_evaluacion} type={evaluation.tipo_evaluacion} descripcion_evaluacion={evaluation.descripcion_evaluacion} />
-          ))
-        }
+          {
+            evaluations.length === 0 ? <h4 className="text-center">No hay evaluaciones pendientes</h4> :
+              evaluations.map((evaluation,) => (
+                <ActivityItem key={evaluation.id_evaluacion} nombre={evaluation.nombre_evaluacion} type={evaluation.tipo_evaluacion} descripcion_evaluacion={evaluation.descripcion_evaluacion} />
+              ))
+          }
 
         </div>
       </CardContent>
