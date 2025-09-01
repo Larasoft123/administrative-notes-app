f2a7f97e782c7002333435955546269838453f3a
Moises Lara <Moisesdavidlara1@gamil.com>
2025-09-01 18:08:09 -0400

    fix whiteSpaces

--- src/app/(home)/estudiantes/page.tsx
+++ src/app/(home)/estudiantes/page.tsx
@@ -1,15 +1,15 @@
-import {StudentsPage} from "@/components/students/students-page"
-import {StudentFilters} from "@/components/students/types"
+import { StudentsPage } from "@/components/students/students-page"
+import { StudentFilters } from "@/components/students/types"
 
 
 
-export default async function Page({searchParams }: {searchParams : StudentFilters}) {
-      const params = await searchParams
-   
-      
+export default async function Page({ searchParams }: { searchParams: StudentFilters }) {
+    const params = (await searchParams)
+
+
     return (
         <div className="min-h-screen">
-            <StudentsPage searchParams={params} />   
+            <StudentsPage searchParams={params} />
         </div>
     )
 }
