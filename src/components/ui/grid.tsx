8274abd6713ffcd4116bec85d489f1ad4e05a64f
Moises Lara <Moisesdavidlara1@gamil.com>
2025-09-06 17:00:50 -0400

    add classname

--- src/components/ui/grid.tsx
+++ src/components/ui/grid.tsx
@@ -1,8 +1,8 @@
 
 
-export default function Grid({ children }: { children: React.ReactNode }) {
+export default function Grid({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <div className="grid gap-4 sm:gap-6 grid-cols-1 p-4 lg:grid-cols-2 xl:grid-cols-3">
+        <div className={`grid gap-4 sm:gap-6 grid-cols-1 p-4 lg:grid-cols-2 xl:grid-cols-3 ${className}`} >
             {children}
         </div>
     )
